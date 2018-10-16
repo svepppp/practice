3a05c96d2d24e15491513e7091c981dddd40a35a
Ilya Shishkov <shishkovilja@yandex.ru>
2018-10-16 23:55:06 +0300

    feature/ba-0002 Added role entity class structure

--- src/main/java/io/khasang/ba/entity/Role.java
+++ src/main/java/io/khasang/ba/entity/Role.java
@@ -1,4 +1,47 @@
 package io.khasang.ba.entity;
 
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+/**
+ * Role entity class. Provides spring security roles in "Business Assistant" project.
+ * Each role must have unique name
+ */
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NaturalId
+    private String name;
+
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
