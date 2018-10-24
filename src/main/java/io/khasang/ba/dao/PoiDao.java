91de9a4f2d5963c2512856224a03fb9f39631916
Mozgachev I.V <mozgachev.ivan@gmail.com>
2018-10-25 01:09:41 +0300

    feature/ba-0010/ticket-01 add dao interface for Poi

---
+++ src/main/java/io/khasang/ba/dao/PoiDao.java
@@ -0,0 +1,6 @@
+package io.khasang.ba.dao;
+
+import io.khasang.ba.entity.Poi;
+
+public interface PoiDao extends BasicDao<Poi> {
+}
---
+++ src/main/java/io/khasang/ba/dao/impl/PoiDaoImpl.java
@@ -0,0 +1,10 @@
+package io.khasang.ba.dao.impl;
+
+import io.khasang.ba.dao.PoiDao;
+import io.khasang.ba.entity.Poi;
+
+public class PoiDaoImpl extends BasicDaoImpl<Poi> implements PoiDao {
+    public PoiDaoImpl(Class<Poi> entityClass) {
+        super(entityClass);
+    }
+}
